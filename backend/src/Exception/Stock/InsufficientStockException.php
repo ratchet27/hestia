532edb5757ef9ac016fd6885966c52fa6bf5936f
Pavel Lanchev <ratchet270@gmail.com>
2026-01-18 19:52:20 +0500

    feat(stock): add InsufficientStockException
    Signed-off-by: Pavel Lanchev <ratchet270@gmail.com>

---
+++ backend/src/Exception/Stock/InsufficientStockException.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace App\Exception\Stock;
+
+use App\Exception\ApiException;
+use App\Exception\ApiProblem;
+
+class InsufficientStockException extends ApiException
+{
+    public function __construct(int $requested, int $available)
+    {
+        parent::__construct(new ApiProblem(
+            title: 'Insufficient stock',
+            type: 'INSUFFICIENT_STOCK',
+            code: 400,
+            extraData: [
+                'requested' => $requested,
+                'available' => $available
+            ]
+        ));
+    }
+}
