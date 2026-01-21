41e9e5d962401e7bc3cd6290e0f24555be0ae63d
Pavel Lanchev <ratchet270@gmail.com>
2026-01-21 15:51:23 +0500

    refactor(frontend): remove shopping list mock data and context
    Signed-off-by: Pavel Lanchev <ratchet270@gmail.com>

--- frontend/src/data/hooks.ts
+++ frontend/src/data/hooks.ts
@@ -6,19 +6,9 @@ import {
   type ChoresContextValue,
   RecipesContext,
   type RecipesContextValue,
-  ShoppingContext,
-  type ShoppingContextValue,
   TasksContext,
   type TasksContextValue,
 } from "./context";
-
-export function useShoppingList(): ShoppingContextValue {
-  const context = useContext(ShoppingContext);
-  if (!context) {
-    throw new Error("useShoppingList must be used within a DataProvider");
-  }
-  return context;
-}
 
 export function useChores(): ChoresContextValue {
   const context = useContext(ChoresContext);
