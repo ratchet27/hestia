3d84345b33bb977a3507576359cf0968a0fcb897
Pavel Lanchev <ratchet270@gmail.com>
2026-06-02 11:57:48 +0500

    feat(auth): app:user:create and app:user:set-password commands
    Signed-off-by: Pavel Lanchev <ratchet270@gmail.com>

---
+++ backend/src/Command/CreateUserCommand.php
@@ -0,0 +1,70 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace App\Command;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+#[AsCommand(name: 'app:user:create', description: 'Create a user account')]
+final class CreateUserCommand extends Command
+{
+    public function __construct(
+        private readonly EntityManagerInterface $em,
+        private readonly UserPasswordHasherInterface $hasher
+    ) {
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->addArgument('username', InputArgument::REQUIRED, 'Login username')
+            ->addOption('name', null, InputOption::VALUE_REQUIRED, 'Display name')
+            ->addOption('email', null, InputOption::VALUE_REQUIRED, 'Email (optional)')
+            ->addOption('password', null, InputOption::VALUE_REQUIRED, 'Password (prompted if omitted)');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+
+        $username = (string) $input->getArgument('username');
+        $name = (string) ( $input->getOption('name') ?? $username );
+        $email = $input->getOption('email');
+        $password = $input->getOption('password') ?? $io->askHidden('Password');
+
+        if (!is_string($password) || $password === '') {
+            $io->error('Password is required.');
+
+            return Command::FAILURE;
+        }
+
+        if ($this->em->getRepository(User::class)->findOneBy(['username' => $username]) !== null) {
+            $io->error(sprintf('User "%s" already exists.', $username));
+
+            return Command::FAILURE;
+        }
+
+        $user = new User($username);
+        $user->setName($name);
+        $user->setEmail(is_string($email) ? $email : null);
+        $user->setPassword($this->hasher->hashPassword($user, $password));
+
+        $this->em->persist($user);
+        $this->em->flush();
+
+        $io->success(sprintf('User "%s" created.', $username));
+
+        return Command::SUCCESS;
+    }
+}
---
+++ backend/tests/Functional/Command/CreateUserCommandTest.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace App\Tests\Functional\Command;
+
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\Console\Tester\CommandTester;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Zenstruck\Foundry\Test\Factories;
+use Zenstruck\Foundry\Test\ResetDatabase;
+
+class CreateUserCommandTest extends KernelTestCase
+{
+    use Factories;
+    use ResetDatabase;
+
+    public function testCreatesUserWithHashedPassword(): void
+    {
+        $application = new Application(self::bootKernel());
+        $tester = new CommandTester($application->find('app:user:create'));
+
+        $tester->execute([
+            'username' => 'pavel',
+            '--name' => 'Pavel',
+            '--email' => 'p@example.com',
+            '--password' => 'secret123'
+        ]);
+
+        $tester->assertCommandIsSuccessful();
+
+        $em = self::getContainer()->get('doctrine')->getManager();
+        $user = $em->getRepository(User::class)->findOneBy(['username' => 'pavel']);
+        static::assertNotNull($user);
+        static::assertSame('Pavel', $user->getName());
+
+        $hasher = self::getContainer()->get(UserPasswordHasherInterface::class);
+        static::assertTrue($hasher->isPasswordValid($user, 'secret123'));
+    }
+}
